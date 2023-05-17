5e7b55de5116fe76258827e7a558273db829024d
hridoyfahad30 <hridoyfahad30@gmail.com>
2023-05-17 22:05:46 +0600

    Initial Build,  React Router & CSS Fremwork Installed

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello world!</div>,
+    },
+  ]);
+
+
+  export default router;
