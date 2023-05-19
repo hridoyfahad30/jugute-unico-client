b36f91896d2d5ab8b5088284e173c9421117639d
hridoyfahad30 <hridoyfahad30@gmail.com>
2023-05-19 20:07:59 +0600

    Add A Toy Page All Functionality Done

--- src/Pages/AddToy/AddToy/AddToy.jsx
+++ src/Pages/AddToy/AddToy/AddToy.jsx
@@ -1,34 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import Swal from "sweetalert2";
 
 const AddToy = () => {
+  const [brand, setBrand] = useState("");
+  const handleBrandChange = (e) => {
+    setBrand(e.target.value);
+  };
 
-    const handleAddToy = (e) => {
-        e.preventDefault();
+  console.log(brand);
 
-        const form = e.target;
+  const handleAddToy = (e) => {
+    e.preventDefault();
 
-        const name = form.name.value;
-        const photo = form.photo.value;
-        const sellerName = form.sellerName.value;
-        const sellerEmail = form.sellerEmail.value;
-        const subcategory = form.subcategory.value;
-        const price = form.price.value;
-        const rating = form.rating.value;
-        const quantity = form.quantity.value;
-        const details = form.details.value;
+    const form = e.target;
 
-        const toy = {name, photo, sellerName, sellerEmail, subcategory, price, rating, quantity, details};
-        
+    const name = form.name.value;
+    const photo = form.photo.value;
+    const sellerName = form.sellerName.value;
+    const sellerEmail = form.sellerEmail.value;
+    const subcategory = form.subcategory.value;
+    const price = form.price.value;
+    const rating = form.rating.value;
+    const quantity = form.quantity.value;
+    const details = form.details.value;
 
+    const newToy = {
+      name,
+      photo,
+      sellerName,
+      sellerEmail,
+      subcategory,
+      price,
+      rating,
+      quantity,
+      details,
+      brand,
+    };
 
-    }
+    const options = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newToy),
+    };
 
+    fetch("http://localhost:5000/toys", options)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        if (response.insertedId) {
+          Swal.fire("Toy Added Successfully");
+        }
+      })
+      .catch((err) => console.error(err));
+  };
 
   return (
     <div>
-      <h1 className="text-2xl md:text-4xl text-center font-semibold text-green-600">Add a Toy</h1>
+      <h1 className="text-2xl md:text-4xl text-center font-semibold text-green-600">
+        Add a Toy
+      </h1>
       <form onSubmit={handleAddToy}>
-        
         <div className="md:flex gap-10 items-center justify-center md:my-10 ">
           <div className="form-control">
             <label className="label">
@@ -45,7 +76,9 @@ const AddToy = () => {
           </div>
           <div className="form-control">
             <label className="label">
-              <span className="label-text text-xl md:text-2xl">Toy Picture URL :</span>
+              <span className="label-text text-xl md:text-2xl">
+                Toy Picture URL :
+              </span>
             </label>
             <label className="input-group">
               <input
@@ -57,11 +90,13 @@ const AddToy = () => {
             </label>
           </div>
         </div>
-      
+
         <div className="md:flex gap-10 items-center justify-center md:my-10 ">
           <div className="form-control">
             <label className="label">
-              <span className="label-text text-xl md:text-2xl">Seller  Name:</span>
+              <span className="label-text text-xl md:text-2xl">
+                Seller Name:
+              </span>
             </label>
             <label className="input-group">
               <input
@@ -74,7 +109,9 @@ const AddToy = () => {
           </div>
           <div className="form-control">
             <label className="label">
-              <span className="label-text text-xl md:text-2xl">Seller Email:</span>
+              <span className="label-text text-xl md:text-2xl">
+                Seller Email:
+              </span>
             </label>
             <label className="input-group">
               <input
@@ -86,11 +123,13 @@ const AddToy = () => {
             </label>
           </div>
         </div>
-   
+
         <div className="md:flex gap-10 items-center justify-center md:my-10 ">
           <div className="form-control">
             <label className="label">
-              <span className="label-text text-xl md:text-2xl">Sub-category:</span>
+              <span className="label-text text-xl md:text-2xl">
+                Sub-category:
+              </span>
             </label>
             <label className="input-group">
               <input
@@ -107,7 +146,7 @@ const AddToy = () => {
             </label>
             <label className="input-group">
               <input
-                type="text"
+                type="number"
                 name="price"
                 placeholder="Price"
                 className="input input-bordered w-96"
@@ -131,12 +170,13 @@ const AddToy = () => {
           </div>
           <div className="form-control">
             <label className="label">
-              <span className="label-text text-xl md:text-2xl">Available Quantity
-:</span>
+              <span className="label-text text-xl md:text-2xl">
+                Available Quantity :
+              </span>
             </label>
             <label className="input-group">
               <input
-                type="text"
+                type="number"
                 name="quantity"
                 placeholder="Available Quantity
 "
@@ -149,8 +189,28 @@ const AddToy = () => {
         <div className="md:flex gap-10 items-center justify-center md:my-10 mb-6">
           <div className="form-control">
             <label className="label">
-              <span className="label-text text-xl md:text-2xl">Detail Description
-:</span>
+              <span className="label-text text-xl md:text-2xl">Brand:</span>
+            </label>
+            <select
+              value={brand}
+              onChange={handleBrandChange}
+              className="select select-success w-96 md:w-[800px]"
+            >
+              <option>Chose A Brand</option>
+
+              <option className="text-xl">BMW</option>
+              <option className="text-xl">Ferrari</option>
+              <option className="text-xl">Mercedes</option>
+            </select>
+          </div>
+        </div>
+
+        <div className="md:flex gap-10 items-center justify-center md:my-10 mb-6">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text text-xl md:text-2xl">
+                Detail Description:
+              </span>
             </label>
             <label className="input-group">
               <input
@@ -163,8 +223,13 @@ const AddToy = () => {
             </label>
           </div>
         </div>
-        <input type="submit" className="btn bg-green-600 hover:bg-green-500 w-full" value="ADD Toy" />
-
+        <div className="flex justify-center">
+          <input
+            type="submit"
+            className="btn bg-green-600 hover:bg-green-500 w-96 md:w-[800px]"
+            value="ADD Toy"
+          />
+        </div>
       </form>
     </div>
   );
